85f0dad2e1c90816742afbfa271dd42f966e2654
cara-puce <E129010D@S042V2pc04.ensinfo.sciences.univ-nantes.prive>
2016-03-03 13:56:42 +0100

    vue bulletin

--- vues.sql
+++ vues.sql
@@ -1,8 +1,9 @@
 CREATE OR REPLACE VIEW Note_etu_globale AS 
-	SELECT ne.annee, ne.noEtu, ne.nomEtu, ne.preEtu, ne.matiere, ne.noteCC, ne.noteExam, nm.moyenneMat 
-	FROM (NoteEtu NATURAL JOIN Etudiant) as ne, NoteMatiere nm 
-	WHERE ne.noteCC = nm.noteCC and  ne.noteExam = nm.noteExam 
-	ORDER BY ne.annee ASC, ne.nomEtu ASC;
+	SELECT ne.annee, ne.noEtu, e.nomEtu, e.preEtu, ne.matiere, ne.noteCC, ne.noteExam, nm.moyenneMat 
+	FROM NoteEtu ne, Etudiant e, NoteMatiere nm
+	WHERE ne.noEtu = e.noEtu
+	AND	ne.noteCC = nm.noteCC and  ne.noteExam = nm.noteExam 
+	ORDER BY ne.annee ASC, e.nomEtu ASC;
 
 CREATE OR REPLACE VIEW Nb_h_matiere AS
 	SELECT matiere, nbH_CM, nbH_TD, nbH_TP, (nbH_CM + nbH_TD + nbH_TP) AS nbH_total
@@ -17,5 +18,11 @@ CREATE OR REPLACE VIEW Liste_etu_matiere AS
   	ORDER BY g.annee ASC, rm.idResp_CM ASC, mt.idresp_TD ASC, mt.idResp_TP ASC, g.matiere ASC, g.groupe ASC, e.nomEtu ASC;
   
 CREATE OR REPLACE VIEW Bulletins_etu AS
-	SELECT noEtu, nomEtu, preEtu, matiere, moyenneMat, avg(moyenneMat) as MoyenneMatPromo, moyenneSem, avg(moyenneSem) as MoyenneSemPromo
-	FROM 
+	SELECT ne.annee, e.noEtu, e.nomEtu, e.preEtu, ne.matiere, nm.moyenneMat, avg(nm.moyenneMat) as MoyenneMatPromo, re.moyenneSem, avg(re.moyenneSem) as MoyenneSemPromo
+	FROM Etudiant e, NoteEtu ne, NoteMatiere nm, ResultatEtudiant re
+	WHERE ne.noEtu = e.noEtu and e.noEtu = re.noEtu
+	--AND ne.annee = re.annee
+	AND	ne.noteCC = nm.noteCC and  ne.noteExam = nm.noteExam
+	GROUP BY ne.annee, e.noEtu, e.nomEtu, e.preEtu, ne.matiere, nm.moyenneMat, re.moyenneSem 
+	HAVING avg(nm.moyenneMat) = (SELECT nm.moyenneMat FROM (NoteEtu NATURAL JOIN NoteMatiere))
+	ORDER BY ne.annee ASC, e.nomEtu ASC;
