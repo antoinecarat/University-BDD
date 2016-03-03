4b62ff88d2dae1d2e0cd9fde90d15e4a6d6c4202
Antoine CARAT <E129010D@I011V7pc04.ensinfo.sciences.univ-nantes.prive>
2016-03-03 08:29:38 +0100

    ajout de vues.sql

--- vues.sql
+++ vues.sql
@@ -1,3 +1,12 @@
-CREATE OR REPLACE VIEW Note_etu_globale AS 
-	SELECT ne.noEtu, nomEtu, preEtu, matiere, ne.noteCC, ne.noteExam, moyenneMat 
-	FROM (NoteEtu NATURAL JOIN Etudiant) ne INNER JOIN NoteMatiere nm ON ne.noteCC = nm.noteCC and  ne.noteExam = nm.noteExam ORDER BY nomEtu ASC;
+-- CREATE OR REPLACE VIEW Note_etu_globale AS 
+-- 	SELECT ne.noEtu, ne.nomEtu, ne.preEtu, ne.matiere, ne.noteCC, ne.noteExam, nm.moyenneMat 
+-- 	FROM (NoteEtu NATURAL JOIN Etudiant) as ne INNER JOIN NoteMatiere nm ON ne.noteCC = nm.noteCC and  ne.noteExam = nm.noteExam ORDER BY nomEtu ASC;
+
+CREATE OR REPLACE VIEW Nb_h_matiere AS
+	SELECT matiere, nbH_CM, nbH_TD, nbH_TP, (nbH_CM + nbH_TD + nbH_TP) AS nbH_total
+	FROM Matiere;
+
+CREATE OR REPLACE VIEW Liste_etu_matiere AS
+	SELECT idResp_CM as Enseignant, matiere, groupe, noEtu
+	FROM ResponsableMatiere, MatiereTdTp, GroupeEtu
+	WHERE idResp_CM = idResp_TD AND idResp_ = idResp_TP 
