03c1eb77472b5284e6664a06fad02f3eccff4bbb
Antoine Carat <antoine.carat@gmail.com>
2016-03-04 12:52:25 +0100

    Add roles.sql

--- README.md
+++ README.md
@@ -19,9 +19,9 @@ Database representing University of Nantes behaviour.
 - [ ] insertion d’un étudiant (noEtu, nom, prenom, groupe)
 - [ ] insertion d'une noteCC / noteExam
 - [ ] bulletin annuel / semestriel de l’étudiant avec moyenne (reporting SQLPlus ?)
-- [ ] calcul la moyenne du semestre d'un étudiant
+- [x] calcul la moyenne du semestre d'un étudiant
 - [ ] calcul de la moyenne de la promo sur une matiere
-- [x] calcul de la moyenne de la promo pour le semestre
+- [ ] calcul de la moyenne de la promo pour le semestre
 
 ### Rôles/Droits : 
 - [x] Univ-Admin : Il peut tout faire
---
+++ roles.sql
@@ -0,0 +1,27 @@
+CREATE ROLE Univ_Admin;
+CREATE ROLE Univ_Enseignant;
+CREATE ROLE Univ_Etudiant;
+
+REVOKE ALL FROM Univ_Enseignant;
+REVOKE ALL FROM Univ_Etudiant;
+
+GRANT ALL TO Univ_Admin;
+
+GRANT SELECT, INSERT, UPDATE ON NoteEtu TO Univ_Enseignant;
+GRANT SELECT ON Note_etu_globale TO Univ_Enseignant;
+GRANT SELECT ON Nb_h_matiere TO Univ_Enseignant;
+GRANT SELECT ON Liste_etu_matiere TO Univ_Enseignant;
+GRANT SELECT ON  TO Univ_Enseignant;
+GRANT SELECT ON  TO Univ_Enseignant;
+GRANT SELECT ON  TO Univ_Enseignant;
+
+GRANT SELECT ON Nb_h_matiere TO Univ_Etudiant;
+GRANT SELECT ON Bulletins_etu TO Univ_Etudiant;
+GRANT SELECT ON  TO Univ_Etudiant;
+GRANT SELECT ON  TO Univ_Etudiant;
+
+GRANT Univ_Admin TO L3_20;
+GRANT Univ_Enseignant TO L3_34;
+GRANT Univ_Enseignant TO L3_29;
+GRANT Univ_Etudiant TO L3_32;
+GRANT Univ_Etudiant TO L3_26;
