34129ca74dda30d95d70ce50d4241756074010d5
Antoine Carat <antoine.carat@gmail.com>
2016-03-04 17:14:20 +0100

    Correct roles.sql

--- roles.sql
+++ roles.sql
@@ -11,14 +11,9 @@ GRANT SELECT, INSERT, UPDATE ON NoteEtu TO Univ_Enseignant;
 GRANT SELECT ON Note_etu_globale TO Univ_Enseignant;
 GRANT SELECT ON Nb_h_matiere TO Univ_Enseignant;
 GRANT SELECT ON Liste_etu_matiere TO Univ_Enseignant;
-GRANT SELECT ON  TO Univ_Enseignant;
-GRANT SELECT ON  TO Univ_Enseignant;
-GRANT SELECT ON  TO Univ_Enseignant;
 
 GRANT SELECT ON Nb_h_matiere TO Univ_Etudiant;
 GRANT SELECT ON Bulletins_etu TO Univ_Etudiant;
-GRANT SELECT ON  TO Univ_Etudiant;
-GRANT SELECT ON  TO Univ_Etudiant;
 
 GRANT Univ_Admin TO L3_20;
 GRANT Univ_Enseignant TO L3_34;
