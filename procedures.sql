32900c1e99985745a9ce3f2264ff209730c6b4c3
Antoine Carat <antoine.carat@gmail.com>
2016-03-04 11:26:11 +0100

    Add procedures.sql

---
+++ procedures.sql
@@ -0,0 +1,66 @@
+create or replace procedure insertEtudiant(
+	noEtu Etudiant.noEtu%type,
+	nomEtu Etudiant.nomEtu%type,
+	preEtu Etudiant.preEtu%type,
+	groupe GroupeEtu.groupe%type,
+	annee GroupeEtu.annee%type) as
+begin
+	insert into Etudiant values (noEtu, nomEtu, preEtu);
+	insert into GroupeEtu values (noEtu, groupe, null, annee);
+end;
+/
+
+create or replace procedure insertCM(
+	idResp_CM CM.idResp_CM%type,
+	nomResp_CM CM.nomResp_CM%type,
+	preResp_CM CM.preResp_CM%type) as
+begin
+	insert into CM values(idResp_CM, nomResp_CM, preResp_CM);
+end;
+/
+
+create or replace procedure insertTD(
+	idResp_TD TD.idResp_TD%type,
+	nomResp_TD TD.nomResp_TD%type,
+	preResp_TD TD.preResp_TD%type) as
+begin
+	insert into TD values(idResp_TD, nomResp_TD, preResp_TD);
+end;
+/
+
+create or replace procedure insertTP(
+	idResp_TP TP.idResp_TP%type,
+	nomResp_TP TP.nomResp_TP%type,
+	preResp_TP TP.preResp_TP%type) as
+begin
+	insert into TP values(idResp_TP, nomResp_TP, preResp_TP);
+end;
+/
+
+-- create or replace function calcul_moyenneSem(
+-- 	noEtudiant ResultatEtudiant.noEtu%type,
+-- 	semestre_etu ResultatEtudiant.semestre%type) as
+-- begin
+-- 	DECLARE
+-- 		CURSOR li IS select moyenneMat, coeffMat
+-- 					from ((NoteEtu NATURAL JOIN NoteMatiere) NATURAL JOIN Matiere) 
+-- 					where noEtu = noEtudiant and SUBSTRING(matiere, 2, 1) = semestre_etu;
+-- 		calcul number(4,2);
+-- 		sum_coeff number(4,2);
+-- 	begin
+-- 		for ligne IN li LOOP
+-- 			calcul := calcul + li.moyenneMat * li.coeffMat;
+-- 			sum_coeff := sum_coeff + li.coeffMat;
+--  		END LOOP;	
+--  		calcul := calcul / sum_coeff;
+--  		update ResultatEtudiant set moyenneSem = calcul where noEtu = noEtudiant;
+--  	end;
+-- end;
+-- /
+
+
+
+
+
+
+
